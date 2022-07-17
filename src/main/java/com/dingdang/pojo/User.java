6acf38cb13a7d920f4f67ba8f3150097d4f17b9c
cbdm <qmzn@163.com>
2022-07-17 19:31:26 +0800

    aaa

--- src/main/java/com/dingdang/pojo/User.java
+++ src/main/java/com/dingdang/pojo/User.java
@@ -2,10 +2,16 @@ package com.dingdang.pojo;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class User {
     private Integer id;
     private String userName;
     private String passWord;
+
+
+    private String aaa;
+    private Date date;
 
 }
